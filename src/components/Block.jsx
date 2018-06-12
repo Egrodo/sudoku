5e597e94062eb1384db38c5e391ddd97c0d06ad5
egrodo <egrodo1@gmail.com>
2018-06-12 19:59:09 -0400

    Attempting to fix hoek vuln

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -6,16 +6,16 @@ class Block extends Component {
   constructor() {
     super();
     this.state = {
-      blocks: [
-        { A1: 1 }, { B1: 2 }, { C1: 3 }, { A2: 4 }, { B2: 5 }, { C2: 6 }, { A3: 7 }, { B3: 8 }, { C3: 9 },
-      ],
+      blocks: {
+        A1: 1, B1: 2, C1: 3, A2: 4, B2: 5, C2: 6, A3: 7, B3: 8, C3: 9,
+      },
     };
   }
   render() {
     return (
       <div className={`block ${this.props.name}`}>
         { Object.keys(this.state.blocks).map(name => (
-          <SubBlock key={name} name={name} value={this.state.blocks.name} />
+          <SubBlock key={name} name={name} value={this.state.blocks[name]} />
         ))}
       </div>
     );
