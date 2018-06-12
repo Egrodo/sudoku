4ec966c88b0646ec5698a70b7b82d339009e2f7c
Egrodo <egrodo1@gmail.com>
2018-06-12 12:34:27 -0400

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'normalize.css';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Sudoku
+      </div>
+    );
+  }
+}
+
+export default App;
